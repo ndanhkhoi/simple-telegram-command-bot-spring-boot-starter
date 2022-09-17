b71a36a6778a0fd95ee67cc94daaa673936db471
Nguyễn Đức Anh Khôi <46568026+ndanhkhoi@users.noreply.github.com>
2022-09-17 19:34:49 +0700

    Update hasPermission function to apply new authorized option

--- lib/src/main/java/io/github/ndanhkhoi/telegram/bot/core/SimpleTelegramLongPollingCommandBot.java
+++ lib/src/main/java/io/github/ndanhkhoi/telegram/bot/core/SimpleTelegramLongPollingCommandBot.java
@@ -69,9 +69,12 @@ public class SimpleTelegramLongPollingCommandBot extends TelegramLongPollingBot 
                 return status == ChatMemberStatus.ADMINISTRATOR || status == ChatMemberStatus.CREATOR;
             }
             else if (isMessageInGroup) {
-                boolean isAcceptedGroup = Arrays.stream(botCommand.getAccessGroupIds()).anyMatch(e -> e == chatId);
+                boolean isAcceptedGroup = botCommand.allowAllGroupAccess() || Arrays.stream(botCommand.getAccessGroupIds()).anyMatch(e -> e == chatId);
                 boolean isAcceptedMember = botCommand.getAccessMemberIds().length == 0 || Arrays.stream(botCommand.getAccessMemberIds()).anyMatch(e -> e == userSendId);
                 return botCommand.isAllowAllUserAccess() || (isAcceptedGroup && isAcceptedMember);
+            }
+            else if (botCommand.onlyForGroup()) {
+                return false;
             }
             else if (botCommand.isOnlyForOwner()) {
                 return botProperties.getBotOwnerChatId().contains(String.valueOf(userSendId));
