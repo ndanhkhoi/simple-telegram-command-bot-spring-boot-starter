b9939d9cbfb4d022b9f49eafae30317793a23439
khoinda <khoinda@vnpt.vn>
2023-01-16 14:17:55 +0700

    Lint: Fix sonar lint

--- lib/src/main/java/io/github/ndanhkhoi/telegram/bot/mapper/UpdateMapper.java
+++ lib/src/main/java/io/github/ndanhkhoi/telegram/bot/mapper/UpdateMapper.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 
 /**
@@ -18,7 +17,6 @@ public class UpdateMapper extends ObjectMapper {
         super.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         super.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         super.setDateFormat(new StdDateFormat());
-        super.registerModule(new JavaTimeModule());
     }
 
     @SneakyThrows
